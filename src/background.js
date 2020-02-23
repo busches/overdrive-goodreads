5af3b6b76a15ad7517d6a1ea096ade6639a3104e
Scott Busche <swbusche@gmail.com>
2020-02-23 12:30:17 -0600

    Convert getGoodReadsRating to async

--- src/background.js
+++ src/background.js
@@ -1,12 +1,15 @@
 import secrets from "secrets";
 
+const getGoodReadsRating = async (searchString) => {
+    const response = await fetch(`https://www.goodreads.com/search/index?key=${secrets.goodreadsApiKey}&q=${encodeURIComponent(searchString)}`);
+    const xml = await response.text();
+    const data = (new window.DOMParser()).parseFromString(xml, "text/xml");
+    const rating = data.querySelector('work average_rating').textContent;
+    return rating;
+}
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const searchString = `${request.title} ${request.author}`
-    fetch(`https://www.goodreads.com/search/index?key=${secrets.goodreadsApiKey}&q=${encodeURIComponent(searchString)}`)
-        .then(response => response.text())
-        .then(xml => (new window.DOMParser()).parseFromString(xml, "text/xml"))
-        .then(data => sendResponse(data.querySelector('work average_rating').textContent))
-        .catch(console.error);
-
+    getGoodReadsRating(searchString).then(sendResponse);
     return true;
 });

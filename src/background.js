f9dad0823f591173368cd1fb17f32e678522e71e
Scott Busche <swbusche@gmail.com>
2020-02-23 12:39:48 -0600

    Cache API calls for one week

--- src/background.js
+++ src/background.js
@@ -1,12 +1,17 @@
 import secrets from "secrets";
+import mem from "mem";
 
-const getGoodReadsRating = async (searchString) => {
+const ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
+
+const getGoodReadsRating = mem(async (searchString) => {
     const response = await fetch(`https://www.goodreads.com/search/index?key=${secrets.goodreadsApiKey}&q=${encodeURIComponent(searchString)}`);
     const xml = await response.text();
     const data = (new window.DOMParser()).parseFromString(xml, "text/xml");
     const rating = data.querySelector('work average_rating').textContent;
     return rating;
-}
+}, {
+    maxAge: ONE_WEEK
+});
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const searchString = `${request.title} ${request.author}`

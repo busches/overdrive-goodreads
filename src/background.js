39851c56611838a9df566d0058c0c821d2d983dc
Scott Busche <swbusche@gmail.com>
2020-02-23 13:09:42 -0600

    Handle zero search results

--- src/background.js
+++ src/background.js
@@ -1,20 +1,26 @@
-import secrets from "secrets";
-import mem from "mem";
+import secrets from 'secrets';
+import mem from 'mem';
 
 const ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
 
-const getGoodReadsRating = mem(async (searchString) => {
+const getGoodReadsRating = mem(async searchString => {
     const response = await fetch(`https://www.goodreads.com/search/index?key=${secrets.goodreadsApiKey}&q=${encodeURIComponent(searchString)}`);
     const xml = await response.text();
-    const data = (new window.DOMParser()).parseFromString(xml, "text/xml");
-    const rating = data.querySelector('work average_rating').textContent;
-    return rating;
+    const data = (new window.DOMParser()).parseFromString(xml, 'text/xml');
+    const numberOfResults = data.querySelector('total-results').textContent;
+
+    if (numberOfResults === "0") {
+        return "??";
+    } else {
+        const rating = data.querySelector('work average_rating').textContent;
+        return rating;
+    }
 }, {
     maxAge: ONE_WEEK
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    const searchString = `${request.title} ${request.author}`
+    const searchString = `${request.title} ${request.author}`;
     getGoodReadsRating(searchString).then(sendResponse);
     return true;
 });

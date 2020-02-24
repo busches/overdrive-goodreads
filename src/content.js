557f283c37389f37559773c1e2fd46b4080918e8
Scott Busche <swbusche@gmail.com>
2020-02-23 21:45:28 -0600

    Swap to mutation observer and listen to all the places

--- src/content.js
+++ src/content.js
@@ -21,28 +21,31 @@ const searchGoodReads = (title, author, book) => {
 	);
 };
 
-const getBooks = () => document.querySelectorAll('li.js-titleCard');
+const searchForBooks = container => {
+	container.querySelectorAll('li.js-titleCard').forEach(book => {
+		const title = book.querySelector('h3').title;
+		const author = book.querySelector('.title-author a').title;
 
-const searchForBooks = () => {
-	// TODO Trash this
-	console.log('searching');
-
-	// TODO do this smarter
-	const foundBooks = getBooks();
-	if (foundBooks.length === 0) {
-		setTimeout(searchForBooks, 1000);
-	} else {
-		foundBooks.forEach(book => {
-			const title = book.querySelector('h3').title;
-			const author = book.querySelector('.title-author a').title;
-
-			searchGoodReads(title, author, book);
-		});
-	}
+		searchGoodReads(title, author, book);
+	});
 };
 
-// TODO replace with the library RGH uses
-document.addEventListener('DOMContentLoaded', searchForBooks);
+const startListener = () => {
+	// .js-relatedTitlesContainer is used for "You may also like"
+	// .js-dynamic-content is for all collection pages
+	// .RTLRecommendations is used for "Didn't find what you were looking for?"
+	document.querySelectorAll('.js-relatedTitlesContainer, .js-dynamic-content, .RTLRecommendations').forEach(updatable => {
+		const observer = new MutationObserver(() => {
+			searchForBooks(updatable);
+		});
+		observer.observe(updatable, {childList: true});
+
+		// Search once content is loaded, as there's no guarantee our extension will load before the page already loads in the data
+		searchForBooks(updatable);
+	});
+};
+
+document.addEventListener('DOMContentLoaded', startListener);
 
 // TODO bring in select
 // TODO why does this have a menu icon in chrome

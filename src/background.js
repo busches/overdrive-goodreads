b8d992a55aed48311831e9818876230328b98523
Scott Busche <swbusche@gmail.com>
2020-06-19 22:35:44 -0500

    Add some smarter filtering rules

--- src/background.js
+++ src/background.js
@@ -4,27 +4,56 @@ import mem from 'mem';
 const ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
 
 const getGoodreadsData = mem(async searchString => {
+	const noMatchFound = {
+		rating: '??',
+		searchString
+	};
+
 	// TODO Move this to API class
 	const response = await fetch(`https://www.goodreads.com/search/index?key=${secrets.goodreadsApiKey}&q=${encodeURIComponent(searchString)}`);
 	const xml = await response.text();
 	const data = (new window.DOMParser()).parseFromString(xml, 'text/xml');
-	const numberOfResults = data.querySelector('results-end').textContent; // total-results is sometimes just wrong
+	const numberOfResults = data.querySelector('results-end').textContent; // Total-results is sometimes just wrong
 
-	// Swap to search by title, then attempt to match on author
-	if (numberOfResults !== '1') {
-		return {
-			rating: '??',
-			searchString
-		};
+	if (numberOfResults > 1) {
+		const allResults = data.querySelectorAll('work');
+		const filteredResults = [];
+		allResults.forEach(searchResult => {
+			// A lot of results start with 'Summary' we don't care about those
+			const title = searchResult.querySelector('title').textContent;
+			if (!title.startsWith('Summary')) {
+				filteredResults.push(searchResult);
+			}
+		});
+
+		// If all the remaining results have the same name, prefer the one with more results
+		if (filteredResults.length > 1) {
+			const allTitles = filteredResults.map(searchResult => searchResult.querySelectorAll('title').textContent);
+			const firstTitle = allTitles[0];
+			const allTitlesMatch = allTitles.every(title => title === firstTitle);
+
+			if (allTitlesMatch) {
+				let highestRatedResult = null;
+				let highestRating = 0;
+
+				filteredResults.forEach(searchResult => {
+					const thisRating = parseInt(searchResult.querySelector('ratings_count').textContent, 10);
+					if (thisRating > highestRating) {
+						highestRating = thisRating;
+						highestRatedResult = searchResult;
+					}
+				});
+
+				return getResultMatchData(highestRatedResult, searchString);
+			}
+		} else if (filteredResults.length === 1) {
+			return getResultMatchData(filteredResults[0], searchString);
+		}
+	} else if (numberOfResults === 0) {
+		return noMatchFound;
 	}
 
-	const rating = data.querySelector('work average_rating').textContent;
-	const bookId = data.querySelector('work best_book id').textContent;
-	return {
-		bookId,
-		rating,
-		searchString
-	};
+	return getResultMatchData(data, searchString);
 }, {
 	maxAge: ONE_WEEK
 });
@@ -34,3 +63,13 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	getGoodreadsData(searchString).then(sendResponse);
 	return true;
 });
+
+function getResultMatchData(data, searchString) {
+	const rating = data.querySelector('work average_rating').textContent;
+	const bookId = data.querySelector('work best_book id').textContent;
+	return {
+		bookId,
+		rating,
+		searchString
+	};
+}

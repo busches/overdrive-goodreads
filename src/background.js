08717187fcab7d72752d3f13a507d6c5245559b3
Scott Busche <swbusche@gmail.com>
2022-10-28 20:39:36 -0500

    Ignore more crap title results from GoodReads (#1)

--- src/background.js
+++ src/background.js
@@ -14,16 +14,9 @@ const ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
 // Maybe exclude anything after --, then we'd search this https://www.goodreads.com/search?utf8=%E2%9C%93&q=The+Awakening+Nora+Roberts&search_type=books
 // Exclude books under X ratings?
 
-// Ignore Study Guides
-// https://www.goodreads.com/search?q=The%20Disappearing%20Spoon%20Sam%20Kean
-// And lesson plans https://www.goodreads.com/search?q=Shadow%20of%20the%20Almighty%20Elisabeth%20Elliot
-
 // This one will be tricky
 // https://www.goodreads.com/search?q=A%20Life%20on%20Our%20Planet%20David%20Attenborough
 // https://www.goodreads.com/search?q=The%20Happiness%20Hypothesis%20Jonathan%20Haidt
-
-// Should have narrowed it down, ignore () crap from GoodReads
-// https://www.goodreads.com/search?q=Mrs.%20Jeffries%20Dusts%20for%20Clues%20Emily%20Brightwell
 
 // Maybe easy
 // https://www.goodreads.com/search?q=The%20House%20of%20Silk%20Anthony%20Horowitz
@@ -46,20 +39,36 @@ const getGoodreadsData = mem(async (searchTitle, searchAuthor) => {
 
 	if (numberOfResults > 1) {
 		const allResults = data.GoodreadsResponse.search[0].results[0].work;
-		const filteredResults = [];
-		for (const searchResult of allResults) {
-			// A lot of results start with 'Summary' we don't care about those
+		let filteredResults = allResults.filter(searchResult => {
+			// A lot of results start with 'Summary', etc. we don't care about those
+			const title = searchResult.best_book[0].title[0].toLowerCase();
+
+			return !title.startsWith('summary') &&
+				!title.startsWith('analysis') &&
+				!title.includes('collection') &&
+				!title.includes('sneak peek') &&
+				!title.includes('study guide') &&
+				!title.includes(' / ');
+		}).filter(searchResult => {
 			// Also don't care about books with no ratings
-			const title = searchResult.best_book[0].title[0];
 			const ratingsCount = Number.parseInt(searchResult.ratings_count[0]._, 10);
-			if (!title.toLowerCase().startsWith('summary') && !title.toLowerCase().startsWith('analysis') && !title.toLowerCase().includes('collection') && ratingsCount > 0) {
-				filteredResults.push(searchResult);
-			}
-		}
+			return ratingsCount > 0;
+		});
 
 		console.log(searchString, 'Filtered results count', filteredResults.length);
 
-		// If all the remaining results have the same name, prefer the one with more results
+		// ignore () crap from GoodReads
+		filteredResults = filteredResults.map(result => {
+			let updatedTitle = result.best_book[0].title[0];
+
+			if (updatedTitle.includes('(')) {
+				updatedTitle = updatedTitle.substring(0, updatedTitle.indexOf('(')).trim();
+			}
+
+			result.best_book[0].title[0] = updatedTitle;
+			return result;
+		});
+
 		if (filteredResults.length > 1) {
 			console.log(searchString, 'Filtered results', filteredResults);
 			const allTitles = filteredResults.map(searchResult => {
@@ -70,6 +79,7 @@ const getGoodreadsData = mem(async (searchTitle, searchAuthor) => {
 			const allTitlesMatch = allTitles.every(title => title === firstTitle);
 
 			console.log(searchString, 'All Titles Match', allTitlesMatch);
+			// If all the remaining results have the same name, prefer the one with more results
 			if (allTitlesMatch) {
 				let highestRatedResult = null;
 				let highestRating = 0;
--- utils/build.js
+++ utils/build.js
@@ -1,3 +1,6 @@
+process.env.BABEL_ENV = 'production';
+process.env.NODE_ENV = 'production';
+
 const webpack = require('webpack');
 const config = require('../webpack.config');
 
